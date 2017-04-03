aa7671decaba5a65e1a3342ff62ec3d2264fb6b3
light <kirill.gus@gmail.com>
2017-04-03 21:42:33 +0300

    Add fb server part

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Process application/json
+app.use(bodyParser.json());
+
+// Index route
+app.get('/', (req, res) => {
+    res.send('There is a root of chat bot');
+});
+
+// for Facebook verification
+app.get('/webhook/', (req, res) => {
+    if (req.query['hub.verify_token'] === process.env.AUTH_TOKEN) {
+        res.send(req.query['hub.challenge']);
+    }
+    res.send('Auth error');
+});
+
+// Spin up the server
+app.listen(app.get('port'), () => {
+    console.log('running on port', app.get('port'));
+});
+
+// handler receiving messages
+app.post('/webhook', (req, res) => {
+    let events = req.body.entry[0].messaging;
+    for (let i = 0; i < events.length; i++) {
+        let event = events[i];
+        if (event.message && event.message.text) {
+            sendMessage(event.sender.id, {
+                text: 'Echo: ' + event.message.text
+            });
+        }
+    }
+    res.sendStatus(200);
+});
+
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, (error, response, body) => {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
